335705edf7cd34f1acc853e38c1948d771fc119e
maipal-oscoy <maipal@oscoy.com>
2023-05-05 21:56:05 +0530

    overrides: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,13 @@
-/* eslint-env node */
 module.exports = {
 	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	overrides: [
+		{
+			files: "**/*.test.*",
+			rules: {
+				"@typescript-eslint/no-unsafe-any": "off",
+			},
+		},
+	],
 	parser: "@typescript-eslint/parser",
 	plugins: ["@typescript-eslint"],
 	root: true,
